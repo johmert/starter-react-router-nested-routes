f1c8d048309282f12ce8d9bffdc8eba8759e44b2
johmert <johmert@users.noreply.github.com>
2022-01-15 21:37:01 -0800

    nested routes

--- src/PostList.js
+++ src/PostList.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 
 function PostList() {
+    const { path } = useRouteMatch();
+
     return (
         <Switch>
-            <Route exact path={"/users/:userId/posts"}>
+            <Route exact path={path}>
                 <p>Here are a list of the user's posts</p>
             </Route>
-            <Route path={"/users/:userId/posts/:postId"}>
+            <Route path={`${path}/:postId`}>
                 <p>Here is a single post</p>
             </Route>
         </Switch>
